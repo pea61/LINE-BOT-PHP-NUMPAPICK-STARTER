f40c8434ba17495d93a983ab7ef38462478f7770
pea61 <36658277+pea61@users.noreply.github.com>
2018-03-21 11:00:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 
-			$Topic = "NodeMCU0.9(ESP-12 Module)" ;
+			$Topic = "NodeMCU1" ;
 			getMqttfromlineMsg($Topic,$text);
 			   
 			
